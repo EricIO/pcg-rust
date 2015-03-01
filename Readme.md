aa7b783ef730190036d966381b78c82df9337670
Eric Skoglund <eric@pagefault.se>
2015-03-01 22:07:17 +0100

    First commit. Basic implementation is simply a port of the C pcg32 basic version from https://github.com/imneme/pcg-c-basic.
    I'm not that satisfied with the unsafe part but don't know enough rust yet to refactor the code.

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is an implementation of the PCG Random Number Generator. More information on these random number generators can be found at pcg-random.org.
---
+++ pcg-rust.rs
@@ -0,0 +1,49 @@
+
+#[allow(dead_code)]
+mod pcg {
+
+    struct pcg32_random {
+        state : u64,
+        inc   : u64,
+    }
+
+    static mut PCG32_GLOBAL : pcg32_random = pcg32_random { state : 0x853c49e6748fea9b, inc : 0xda3e39cb94b95bdb };
+
+    pub fn pcg32_srandom_r(rng : &mut pcg32_random, initstate : u64, initseq : u64) {
+        rng.state = 0u64;
+        rng.inc   = (initseq << 1u64) | 1u64;
+        pcg32_random_r(rng);
+        rng.state += initstate;
+        pcg32_random_r(rng);
+    }
+
+    pub unsafe fn pcg32_srandom(seed: u64, seq : u64) -> () {
+        pcg32_srandom_r(&mut PCG32_GLOBAL, seed, seq);
+    }
+
+    pub fn pcg32_random_r(rng : &mut pcg32_random) -> u32 {
+        let oldstate   = rng.state;
+        rng.state      = oldstate * 6364136223846793005u64 + rng.inc;
+        let xorshifted = (((oldstate >> 18) ^ oldstate) >> 27) as u32;
+        let rot        = oldstate >> 59;
+        return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+    }
+
+    pub unsafe fn pcg32_random() -> u32 {
+        return pcg32_random_r(&mut PCG32_GLOBAL);
+    }
+
+    pub fn pcg32_boundedrand_r(rng : &mut pcg32_random, bound : u32) -> u32 {
+        let threshold = -bound % bound;
+        loop {
+            let r = pcg32_random_r(rng);
+            if r >= threshold {
+                return r % bound;
+            }
+        }
+    }
+
+    pub unsafe fn pcg_boundedrand(bound : u32) -> u32 {
+        return pcg32_boundedrand_r(&mut PCG32_GLOBAL, bound);
+    }
+}
